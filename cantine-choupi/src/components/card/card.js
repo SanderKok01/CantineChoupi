2e5af86a282c8805992abfbfa252d3c045ba41f9
SanderKok <sander.jt.kok@gmail.com>
2020-02-26 12:43:13 +0100

    TD 3
    Added Categories section

---
+++ cantine-choupi/src/components/card/card.js
@@ -0,0 +1,97 @@
+import React from 'react';
+import "./card_styles.scss";
+
+import { FaTimes } from 'react-icons/fa';
+import { GoInfo } from 'react-icons/go';
+
+class Card extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.open = this.open.bind(this);
+    this.close = this.close.bind(this);
+    this.setOthersInactive = this.setOthersInactive.bind(this);
+    this.setOthersActive = this.setOthersActive.bind(this);
+
+    this.state = {
+      isOpen: false,
+      click: this.open,
+      classes: {
+        card: "card [ is-collapsed ]",
+        card_inner: "card__inner"
+      },
+      inactiveClass: "is-inactive"
+    };
+  };
+
+  open() {
+    this.setState({
+      click: this.close,
+      classes: {
+        ...this.state.classes,
+        card: "card [ is-expanded ]"
+      }
+    });
+    this.setOthersInactive();
+  };
+
+  close() {
+    this.setState({
+      click: this.open,
+      classes: {
+        ...this.state.classes,
+        card: "card [ is-collapsed ]"
+      }
+    });
+    this.setOthersActive();
+  };
+
+  setOthersInactive() {
+    const allCards = document.querySelector(this.props.wrapper).querySelectorAll('.card');
+    let currentCard;
+    Array.from(allCards).forEach(function(card) {
+      if (card.classList.contains('is-expanded')) {
+        currentCard = card;
+      };
+
+      if (card !== currentCard) {
+        card.classList.add('is-inactive');
+        card.setAttribute('disabled', "true");
+      };
+    });
+  };
+
+  setOthersActive() {
+    const allCards = document.querySelector(this.props.wrapper).querySelectorAll('.card');
+    let currentCard;
+    Array.from(allCards).forEach(function(card) {
+      if (card.disabled) {
+        currentCard = card;
+      };
+
+      if (card !== currentCard) {
+        card.classList.remove('is-inactive');
+        card.removeAttribute('disabled');
+      };
+    });
+  };
+
+  render() {
+    return (
+      <React.Fragment>
+        <div className={ this.state.classes.card }>
+          <div className={ this.state.classes.card_inner } onClick={ this.disabled ? null : this.state.click }>
+            <span>{ this.props.title }</span>
+            <GoInfo className="fa" />
+          </div>
+          <div className="card__expander">
+            <FaTimes className="fa" onClick={ this.close } />
+            { this.props.children }
+          </div>
+        </div>
+      </React.Fragment>
+    );
+  };
+};
+
+export default Card;
--- cantine-choupi/src/pages/landing/landing.js
+++ cantine-choupi/src/pages/landing/landing.js
@@ -4,11 +4,22 @@ import "./landing_styles.scss";
 import Call from "../../helpers/call";
 
 import { Slide } from 'react-slideshow-image';
-import { TiArrowDownOutline } from "react-icons/ti";
+import { TiArrowDownOutline, TiArrowUpOutline } from "react-icons/ti";
 import { Link } from 'react-scroll';
+import Card from '../../components/card/card';
+
+const longLorem = "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium distinctio reiciendis odio nisi esse dicta vero? Ipsa ratione adipisci quisquam. Sunt aliquam accusantium unde dicta tenetur sed veritatis itaque id? Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt beatae quasi quae, ex aperiam excepturi enim perspiciatis quo in mollitia incidunt, ipsa vel dolorum voluptate molestiae laborum eum esse! Officiis! Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi dignissimos explicabo inventore quia consectetur minus ad magni, blanditiis consequatur, incidunt eos itaque commodi tempore. Tempore minima ducimus fuga nemo dignissimos! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium distinctio reiciendis odio nisi esse dicta vero? Ipsa ratione adipisci quisquam. Sunt aliquam accusantium unde dicta tenetur sed veritatis itaque id? Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt beatae quasi quae, ex aperiam excepturi enim perspiciatis quo in mollitia incidunt, ipsa vel dolorum voluptate molestiae laborum eum esse! Officiis! Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi dignissimos explicabo inventore quia consectetur minus ad magni, blanditiis consequatur, incidunt eos itaque commodi tempore. Tempore minima ducimus fuga nemo dignissimos! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium distinctio reiciendis odio nisi esse dicta vero? Ipsa ratione adipisci quisquam. Sunt aliquam accusantium unde dicta tenetur sed veritatis itaque id? Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt beatae quasi quae, ex aperiam excepturi enim perspiciatis quo in mollitia incidunt, ipsa vel dolorum voluptate molestiae laborum eum esse!";
+const shortLorem = "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium distinctio reiciendis odio nisi esse dicta vero? Ipsa ratione adipisci quisquam. Sunt aliquam accusantium unde dicta tenetur sed veritatis itaque id? Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt beatae quasi quae, ex aperiam excepturi enim perspiciatis quo in mollitia incidunt, ipsa vel dolorum voluptate molestiae laborum eum esse! Officiis! Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi dignissimos explicabo inventore quia consectetur minus ad magni, blanditiis consequatur, incidunt eos itaque commodi tempore. Tempore minima ducimus fuga nemo dignissimos! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium distinctio reiciendis odio nisi esse dicta vero? Ipsa ratione adipisci quisquam. Sunt aliquam accusantium unde dicta tenetur sed veritatis itaque id? Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt beatae quasi quae, ex aperiam excepturi enim perspiciatis quo in mollitia incidunt, ipsa vel dolorum voluptate molestiae laborum eum esse! Officiis! Lorem ipsum dolor sit amet consectetur adipisicing elit.";
 
 const carouselProperties = {
   duration: 5000,
+  transitionDuration: 500,
+  infinite: true,
+  indicators: true
+}
+
+const categoriesCarouselProperties = {
+  duration: 10000,
   transitionDuration: 500,
   infinite: true,
   indicators: true
@@ -19,14 +30,31 @@ class Landing extends React.Component {
     super(props);
 
     this.setData = this.setData.bind(this);
+    this.getCategories = this.getCategories.bind(this);
 
     this.state = {
-      dataRes: []
+      dataRes: [],
+      categoriesRes: []
     };
   };
 
   async componentDidMount() {
     await this.setData();
+    await this.getCategories();
+  }
+
+  getCategories() {
+    try {
+      Call('categoriesWithProducts')
+      .then(res => {
+        this.setState({
+          categoriesRes: res
+        });
+      })
+      .catch(console.error);
+    } catch (err) {
+      console.error(err);
+    };
   }
 
   setData() {
@@ -46,7 +74,7 @@ class Landing extends React.Component {
   render() {
     return (
       <div className="landing">
-        <section className="intro">
+        <section className="intro" name="intro">
           <h1 className="intro__title">Cantine Choupi</h1>
           <Link to="dishes" smooth={ true } duration={ 500 }>
             <TiArrowDownOutline className="intro__icon" />
@@ -100,13 +128,51 @@ class Landing extends React.Component {
         <section name="about-us" className="about-us">
           <h3 className="about-us__title">Over Ons</h3>
           <div className="about-us__description-wrapper">
-            <p className="about-us__description about-us__description--long">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium distinctio reiciendis odio nisi esse dicta vero? Ipsa ratione adipisci quisquam. Sunt aliquam accusantium unde dicta tenetur sed veritatis itaque id? Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt beatae quasi quae, ex aperiam excepturi enim perspiciatis quo in mollitia incidunt, ipsa vel dolorum voluptate molestiae laborum eum esse! Officiis! Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi dignissimos explicabo inventore quia consectetur minus ad magni, blanditiis consequatur, incidunt eos itaque commodi tempore. Tempore minima ducimus fuga nemo dignissimos! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium distinctio reiciendis odio nisi esse dicta vero? Ipsa ratione adipisci quisquam. Sunt aliquam accusantium unde dicta tenetur sed veritatis itaque id? Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt beatae quasi quae, ex aperiam excepturi enim perspiciatis quo in mollitia incidunt, ipsa vel dolorum voluptate molestiae laborum eum esse! Officiis! Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi dignissimos explicabo inventore quia consectetur minus ad magni, blanditiis consequatur, incidunt eos itaque commodi tempore. Tempore minima ducimus fuga nemo dignissimos! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium distinctio reiciendis odio nisi esse dicta vero? Ipsa ratione adipisci quisquam. Sunt aliquam accusantium unde dicta tenetur sed veritatis itaque id? Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt beatae quasi quae, ex aperiam excepturi enim perspiciatis quo in mollitia incidunt, ipsa vel dolorum voluptate molestiae laborum eum esse!</p>
-            <p className="about-us__description">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium distinctio reiciendis odio nisi esse dicta vero? Ipsa ratione adipisci quisquam. Sunt aliquam accusantium unde dicta tenetur sed veritatis itaque id? Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt beatae quasi quae, ex aperiam excepturi enim perspiciatis quo in mollitia incidunt, ipsa vel dolorum voluptate molestiae laborum eum esse! Officiis! Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi dignissimos explicabo inventore quia consectetur minus ad magni, blanditiis consequatur, incidunt eos itaque commodi tempore. Tempore minima ducimus fuga nemo dignissimos! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium distinctio reiciendis odio nisi esse dicta vero? Ipsa ratione adipisci quisquam. Sunt aliquam accusantium unde dicta tenetur sed veritatis itaque id? Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt beatae quasi quae, ex aperiam excepturi enim perspiciatis quo in mollitia incidunt, ipsa vel dolorum voluptate molestiae laborum eum esse! Officiis! Lorem ipsum dolor sit amet consectetur adipisicing elit. </p>
-            <p className="about-us__description about-us__description--long">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium distinctio reiciendis odio nisi esse dicta vero? Ipsa ratione adipisci quisquam. Sunt aliquam accusantium unde dicta tenetur sed veritatis itaque id? Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt beatae quasi quae, ex aperiam excepturi enim perspiciatis quo in mollitia incidunt, ipsa vel dolorum voluptate molestiae laborum eum esse! Officiis! Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi dignissimos explicabo inventore quia consectetur minus ad magni, blanditiis consequatur, incidunt eos itaque commodi tempore. Tempore minima ducimus fuga nemo dignissimos! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium distinctio reiciendis odio nisi esse dicta vero? Ipsa ratione adipisci quisquam. Sunt aliquam accusantium unde dicta tenetur sed veritatis itaque id? Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt beatae quasi quae, ex aperiam excepturi enim perspiciatis quo in mollitia incidunt, ipsa vel dolorum voluptate molestiae laborum eum esse! Officiis! Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi dignissimos explicabo inventore quia consectetur minus ad magni, blanditiis consequatur, incidunt eos itaque commodi tempore. Tempore minima ducimus fuga nemo dignissimos! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium distinctio reiciendis odio nisi esse dicta vero? Ipsa ratione adipisci quisquam. Sunt aliquam accusantium unde dicta tenetur sed veritatis itaque id? Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt beatae quasi quae, ex aperiam excepturi enim perspiciatis quo in mollitia incidunt, ipsa vel dolorum voluptate molestiae laborum eum esse!</p>
+            <p className="about-us__description about-us__description--long">{ longLorem }</p>
+            <p className="about-us__description">{ shortLorem }</p>
+            <p className="about-us__description about-us__description--long">{ longLorem }</p>
           </div>
         </section>
-        <Link to="about-us" smooth={ true } duration={ 500 }>
+        <Link to="categories" smooth={ true } duration={ 500 }>
           <TiArrowDownOutline className="about-us__icon" />
+        </Link>
+        <section name="categories" className="categories">
+          <h3 className="categories__title">De Categoriën</h3>
+          <div className="categories__wrapper">
+            {
+              this.state.categoriesRes.length === 0 ? null : this.state.categoriesRes.map((cat, index) => {
+                return (
+                  <React.Fragment key={ index }>
+                    {
+                      cat.products.length === 0 ? null : (
+                        <Card key={ index } title={ cat.name } wrapper=".categories__wrapper">
+                          <div className="categories__carousel__wrapper">
+                            <Slide { ...categoriesCarouselProperties }>
+                              {
+                                cat.products.map((prod, index) => {
+                                  return (
+                                    <div className="categories__carousel__each-fade" key={ index }>
+                                      <div className="categories__carousel__text-wrapper">
+                                        <h2 className="categories__carousel__text-title">{ prod.name }</h2>
+                                        <p className="categories__carousel__text-description">{ prod.description }</p>
+                                      </div>
+                                    </div>
+                                  );
+                                })
+                              }
+                            </Slide>
+                          </div>
+                        </Card>
+                      )
+                    }
+                  </React.Fragment>
+                )
+              })
+            }
+          </div>
+        </section>
+        <Link to="intro" smooth={ true } duration={ 1000 }>
+          <TiArrowUpOutline className="categories__icon" />
         </Link>
       </div>
     );
