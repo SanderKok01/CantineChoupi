6bfd0c3c6958b7bf394ec25ea5db454337ba0968
SanderKok <sander.jt.kok@gmail.com>
2020-02-26 09:42:59 +0100

    TD 4
    About us section

--- cantine-choupi/src/pages/landing/landing.js
+++ cantine-choupi/src/pages/landing/landing.js
@@ -5,6 +5,7 @@ import Call from "../../helpers/call";
 
 import { Slide } from 'react-slideshow-image';
 import { TiArrowDownOutline } from "react-icons/ti";
+import { Link } from 'react-scroll';
 
 const carouselProperties = {
   duration: 5000,
@@ -47,9 +48,11 @@ class Landing extends React.Component {
       <div className="landing">
         <section className="intro">
           <h1 className="intro__title">Cantine Choupi</h1>
-          <TiArrowDownOutline className="intro__icon" />
+          <Link to="dishes" smooth={ true } duration={ 500 }>
+            <TiArrowDownOutline className="intro__icon" />
+          </Link>
         </section>
-        <section className="dishes">
+        <section className="dishes" name="dishes">
           <p className="dishes__title">Onze Producten</p>
           <div className="carousel__wrapper">
               {
@@ -91,6 +94,20 @@ class Landing extends React.Component {
             <p className="dishes__all-products">Geïnteresseerd? Klik <a className="dishes__link" href="##">hier</a> voor alle producten.</p>
           </div>
         </section>
+        <Link to="about-us" smooth={ true } duration={ 500 }>
+          <TiArrowDownOutline className="dishes__icon" />
+        </Link>
+        <section name="about-us" className="about-us">
+          <h3 className="about-us__title">Over Ons</h3>
+          <div className="about-us__description-wrapper">
+            <p className="about-us__description about-us__description--long">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium distinctio reiciendis odio nisi esse dicta vero? Ipsa ratione adipisci quisquam. Sunt aliquam accusantium unde dicta tenetur sed veritatis itaque id? Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt beatae quasi quae, ex aperiam excepturi enim perspiciatis quo in mollitia incidunt, ipsa vel dolorum voluptate molestiae laborum eum esse! Officiis! Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi dignissimos explicabo inventore quia consectetur minus ad magni, blanditiis consequatur, incidunt eos itaque commodi tempore. Tempore minima ducimus fuga nemo dignissimos! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium distinctio reiciendis odio nisi esse dicta vero? Ipsa ratione adipisci quisquam. Sunt aliquam accusantium unde dicta tenetur sed veritatis itaque id? Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt beatae quasi quae, ex aperiam excepturi enim perspiciatis quo in mollitia incidunt, ipsa vel dolorum voluptate molestiae laborum eum esse! Officiis! Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi dignissimos explicabo inventore quia consectetur minus ad magni, blanditiis consequatur, incidunt eos itaque commodi tempore. Tempore minima ducimus fuga nemo dignissimos! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium distinctio reiciendis odio nisi esse dicta vero? Ipsa ratione adipisci quisquam. Sunt aliquam accusantium unde dicta tenetur sed veritatis itaque id? Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt beatae quasi quae, ex aperiam excepturi enim perspiciatis quo in mollitia incidunt, ipsa vel dolorum voluptate molestiae laborum eum esse!</p>
+            <p className="about-us__description">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium distinctio reiciendis odio nisi esse dicta vero? Ipsa ratione adipisci quisquam. Sunt aliquam accusantium unde dicta tenetur sed veritatis itaque id? Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt beatae quasi quae, ex aperiam excepturi enim perspiciatis quo in mollitia incidunt, ipsa vel dolorum voluptate molestiae laborum eum esse! Officiis! Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi dignissimos explicabo inventore quia consectetur minus ad magni, blanditiis consequatur, incidunt eos itaque commodi tempore. Tempore minima ducimus fuga nemo dignissimos! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium distinctio reiciendis odio nisi esse dicta vero? Ipsa ratione adipisci quisquam. Sunt aliquam accusantium unde dicta tenetur sed veritatis itaque id? Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt beatae quasi quae, ex aperiam excepturi enim perspiciatis quo in mollitia incidunt, ipsa vel dolorum voluptate molestiae laborum eum esse! Officiis! Lorem ipsum dolor sit amet consectetur adipisicing elit. </p>
+            <p className="about-us__description about-us__description--long">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium distinctio reiciendis odio nisi esse dicta vero? Ipsa ratione adipisci quisquam. Sunt aliquam accusantium unde dicta tenetur sed veritatis itaque id? Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt beatae quasi quae, ex aperiam excepturi enim perspiciatis quo in mollitia incidunt, ipsa vel dolorum voluptate molestiae laborum eum esse! Officiis! Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi dignissimos explicabo inventore quia consectetur minus ad magni, blanditiis consequatur, incidunt eos itaque commodi tempore. Tempore minima ducimus fuga nemo dignissimos! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium distinctio reiciendis odio nisi esse dicta vero? Ipsa ratione adipisci quisquam. Sunt aliquam accusantium unde dicta tenetur sed veritatis itaque id? Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt beatae quasi quae, ex aperiam excepturi enim perspiciatis quo in mollitia incidunt, ipsa vel dolorum voluptate molestiae laborum eum esse! Officiis! Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi dignissimos explicabo inventore quia consectetur minus ad magni, blanditiis consequatur, incidunt eos itaque commodi tempore. Tempore minima ducimus fuga nemo dignissimos! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium distinctio reiciendis odio nisi esse dicta vero? Ipsa ratione adipisci quisquam. Sunt aliquam accusantium unde dicta tenetur sed veritatis itaque id? Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt beatae quasi quae, ex aperiam excepturi enim perspiciatis quo in mollitia incidunt, ipsa vel dolorum voluptate molestiae laborum eum esse!</p>
+          </div>
+        </section>
+        <Link to="about-us" smooth={ true } duration={ 500 }>
+          <TiArrowDownOutline className="about-us__icon" />
+        </Link>
       </div>
     );
   }
